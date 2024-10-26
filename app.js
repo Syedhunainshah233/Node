3781772fad4ff428ae52b5ced319ec98f89d9b8d
Hunain <syedhunainshah120@gmail.com>
2024-10-26 01:25:29 -0700

    Update File

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const Host = "127.0.0.1";
 app.get("/" , (req , res)=>{
     res.status(200).send("This Is Home Page")
 })
+app.get("/Services" , (req , res)=>{
+    res.status(200).send("This Is Services Page")
+})
+app.get("/About" , (req , res)=>{
+    res.status(200).send("This Is Abouts Page")
+})
+app.get("/Contant" , (req , res)=>{
+    res.status(200).send("This Is Contant Page")
+})
 
 // server Online Code
 app.listen(Port , ()=>{
